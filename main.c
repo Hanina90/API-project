cf8ea060effbb80f966795da31b101d5e8e062e4
Hanina90 <38525420+Hanina90@users.noreply.github.com>
2018-05-22 12:39:48 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 //--------------------------------------------------------------
 // File     : main.c
-// Datum    : 10.05.2018
+// Datum    : 30.03.2016
 // Version  : 1.0
-// Autor    : Raha Hanina
+// Autor    : UB
 // mods by	: J.F. van der Bent
 // CPU      : STM32F4
 // IDE      : CooCox CoIDE 1.7.x
@@ -14,144 +14,91 @@
 #include "stm32_ub_vga_screen.h"
 #include <math.h>
 
-float x_1_ = 100;
-float x_2_ = 200;
-float x_3_ = 10;
-float y_1_ = 20;
-float y_2_ = 20;
-float y_3_ = 100;
+float x_begin_ = 100;
+float x_eind_ = 100;
+float y_begin_ = 50;
+float y_eind_ = 150;
+int kleur_ = 100;
+int dikte_ = 3;
 
-int kleur_ = 0;
-
-void driehoek (float x_1, float x_2, float x_3, float y_1, float y_2, float y_3, float kleur);
+void lijn( float x_begin, float x_eind, float y_begin,float y_eind,int dikte, int kleur );
 
 int main(void)
 {
+
+	//  uint32_t n;
 
 	SystemInit(); // System speed to 168MHz
 
 	UB_VGA_Screen_Init(); // Init VGA-Screen
 
-    UB_VGA_FillScreen(VGA_COL_GREEN);
-    driehoek (x_1_, x_2_, x_3_, y_1_, y_2_, y_3_, kleur_);
+	UB_VGA_FillScreen(VGA_COL_GREEN);
 
-while(1)
-  {
-  }
+	lijn( x_begin_, x_eind_,  y_begin_, y_eind_, dikte_ , kleur_);
+
+
+	while(1)
+		  	{
+		  	}
 }
 
-void driehoek (float x_1, float x_2, float x_3, float y_1, float y_2, float y_3, float kleur){
+void lijn( float x_begin, float x_eind, float y_begin,float y_eind,int dikte, int kleur ) {
 
-////////////////////////////////////x1 en x2////////////////////////////////////////////////////////////////
-	    if (x_2 > x_1)
-	   	  {
+	if (x_eind > x_begin)
+	{
 
-	   		  for(int x = x_1; (x <= x_2); x++)
-	   		  {
-	   				  float y = ((y_2 - y_1)/ (x_2- x_1))* (x - x_1) + y_1;
-	   				  UB_VGA_SetPixel(x,y,kleur);
-	   		  }
-	   	  }
+		for(int x = x_begin; (x <= x_eind); x++)
+		{
+			for(int i = 0; i < dikte; i++)
+			{
+				//float y = (a * x) + b;
 
-	   	  if (x_2 < x_1)
-	   	  {
-	   		  for(int x = x_2; (x <= x_1); x++)
-	   		  	  {
-	   		  			  float y = ((y_2 - y_1)/ (x_2 - x_1))* (x - x_1) + y_1;
-	   		  			  UB_VGA_SetPixel(x,y,kleur);
-	   		  	  }
-	   	  }
+				float y = ((y_eind - y_begin)/ (x_eind - x_begin))* (x - x_begin) + y_begin;
+				UB_VGA_SetPixel(x,(y+i), kleur);
+			}
+		}
+	}
 
-	   	  if (x_1 == x_2 && y_2 > y_1)
-	   	  {
-	   		  for(int y = y_1; (y <= y_2); y++)
-	   		  	  	  {
-	   		  	  			float  x = x_1;
-	   		  	  		    UB_VGA_SetPixel(x,y,kleur);
-	   		  	  	  }
-	   	  }
+	if (x_eind < x_begin)
+	{
+		for(int x = x_eind; (x <= x_begin); x++)
+		{
+			for(int i = 0; i < dikte; i++)
+			{
+				//float y = (a * x) + b;
 
-	   	  if (x_1 == x_2 && y_2 < y_1)
-	   	    {
-	   	  	  for(int y = y_2; (y <= y_1); y++)
-	   	  	  	  	  {
-	   	  	  	  			float  x = x_1;
-	   	  	  	  	        UB_VGA_SetPixel(x,y,kleur);
-	   	  	  	  	  }
-	   	    }
-////////////////////////////x2 en x3//////////////////////////////////////////////////////////
-	   	 if (x_3 > x_2)
-	   		  {
+				float y = ((y_eind - y_begin)/ (x_eind - x_begin))* (x - x_begin) + y_begin;
+				UB_VGA_SetPixel(x,(y+i), kleur);
+			}
+		}
+	}
 
-	   			  for(int x = x_2; (x <= x_3); x++)
-	   			  {
-	   					  float y = ((y_3 - y_2)/ (x_3 - x_2))* (x - x_2) + y_2;
-	   					  UB_VGA_SetPixel(x, y, kleur);
-	   			  }
-	   		  }
+	if (x_begin == x_eind && y_eind > y_begin)
+	{
+				  for(int y = y_begin; (y <= y_eind); y++)
+				  	  	  {
+				  	  		  for(int i = 0; i < dikte; i++)
+				  	  		  {
+				  	  			float  x = x_begin;
+				  	  			UB_VGA_SetPixel(x,(y+i), kleur);
+				  	  		  }
+				  	  	  }
+	}
 
-	   		  if (x_3 < x_2)
-	   		  {
-	   			  for(int x = x_3; (x <= x_2); x++)
-	   			  	  {
-	   			  			  float y = ((y_3 - y_2)/ (x_3 - x_2))* (x - x_2) + y_2;
-	   			  			  UB_VGA_SetPixel(x, y, kleur);
-	   			  	  }
-	   		  }
+			  if (x_begin == x_eind && y_eind < y_begin)
+			    {
+			  	  for(int y = y_eind; (y <= y_begin); y++)
+			  	  	  	  {
+			  	  	  		  for(int i = 0; i < dikte; i++)
+			  	  	  		  {
+			  	  	  			float  x = x_begin;
+			  	  	  			UB_VGA_SetPixel(x,(y+i), kleur);
+			  	  	  		  }
+			  	  	  	  }
+			    }
 
-	   		  if (x_2 == x_3 && y_3 > y_2)
-	   		  {
-	   			  for(int y = y_2; (y <= y_3); y++)
-	   			  	  	  {
-	   			  	  			float  x = x_2;
-	   			  	  			UB_VGA_SetPixel(x, y, kleur);
-	   			  	  	  }
-	   		  }
-
-	   		  if (x_2 == x_3 && y_3 < y_2)
-	   		    {
-	   		  	  for(int y = y_3; (y <= y_2); y++)
-	   		  	  	  	  {
-	   		  	  	  			float  x = x_2;
-	   		  	  	  			UB_VGA_SetPixel(x, y, kleur);
-	   		  	  	  	  }
-	   		    }
-//////////////////////////////////////x1 en x3///////////////////////////////////////////////////////////
-	   		 if (x_3 > x_1)
-	   			  {
-
-	   				  for(int x = x_1; (x <= x_3); x++)
-	   				  {
-	   						  float y = ((y_3 - y_1)/ (x_3 - x_1))* (x - x_1) + y_1;
-	   						  UB_VGA_SetPixel(x, y, kleur);
-	   				  }
-	   			  }
-
-	   			  if (x_3 < x_1)
-	   			  {
-	   				  for(int x = x_3; (x <= x_1); x++)
-	   				  	  {
-	   				  			  float y = ((y_3 - y_1)/ (x_3 - x_1))* (x - x_1) + y_1;
-	   				  			  UB_VGA_SetPixel(x, y, kleur);
-	   				  	  }
-	   			  }
-
-	   			  if (x_1 == x_3 && y_3 > y_1)
-	   			  {
-	   				  for(int y = y_1; (y <= y_3); y++)
-	   				  	  	  {
-	   				  	  			float  x = x_1;
-	   				  	  		    UB_VGA_SetPixel(x, y, kleur);
-	   				  	  	  }
-	   			  }
-
-	   			  if (x_1 == x_3 && y_3 < y_1)
-	   			    {
-	   			  	  for(int y = y_3; (y <= y_1); y++)
-	   			  	  	  	  {
-	   			  	  	  			float  x = x_1;
-	   			  	  	  	        UB_VGA_SetPixel(x ,y, kleur);
-	   			  	  	  	  }
-	   			    }
 }
+
+
+
 
